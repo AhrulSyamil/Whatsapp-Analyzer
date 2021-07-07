4462d7a8e55ddf9a34bf6dfad242084a9b30d13c
AhrulSyamil <ahrulsyamilulum@gmail.com>
2021-07-07 14:37:08 +0700

    update

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from io import BytesIO
 import base64
 from flask import Flask, render_template, request, json, Response
+from flask_cors import CORS
 
 from chat_analytic import chatAnalytics
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def index() :
     return render_template('index.html')
 
-@app.route('/result', methods=["GET", "POST"])
+@app.route('/test', methods=['GET'])
+def test() :
+    return "test"
+
+@app.route('/result', methods=["POST"])
 def result() :
     request_file = request.files['whatsapp_file']
     
@@ -28,7 +34,7 @@ def result() :
                 'total_chat': chat_whatsapp.total_chat(),
                 'content_chat': chat_whatsapp.content(),
                 'timeline': chat_whatsapp.timeline(),
-                'sentiment_analysis': chat_whatsapp.sentiment_analysis()
+                # 'sentiment_analysis': chat_whatsapp.sentiment_analysis()
             }),
             status = 200,
             mimetype = 'application/json'
