9c2e83c94db73f3515d76a7b41bdfda38680851f
alif-syhm01 <aliffio.syahm01@gmail.com>
2021-07-07 07:54:11 +0700

    Add some feature

---
+++ chat_analytic.py
@@ -0,0 +1,192 @@
+from datetime import date, datetime
+import re
+import emoji
+import datefinder
+import collections
+
+class chatAnalytics :
+
+    #initial for ios version
+    is_android = False;
+    is_ios = False;
+
+    def __init__(self, text_file) :
+        self.__file = text_file.read().decode()
+        self.__pattern = re.compile('\d+:\d+\s+-\s+([a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?[a-zA-Z0-9]+\s?):\s+')
+        self.__messengers = re.findall(self.__pattern, self.__file)
+
+    def check_device(self) :
+        if (self.__file[0] == '[') :
+            self.is_ios = True
+
+            return {
+                "device_os": "iOS",
+                "device_name": "iPhone",
+                "status": self.is_ios
+            }
+        else :
+            self.is_android = True
+            
+            return {
+                "device_os": "Android",
+                "device_name": "Android",
+                "status": self.is_android
+            }
+
+    def detail_chat(self) :
+        if (self.__file[0] == '[') :
+            message_split = self.__pattern.split(self.__file)
+            return message_split
+        else :
+            messengers = re.findall(self.__pattern, self.__file)
+
+            count_messages = {}
+            for each in messengers :
+                if each in count_messages.keys() :
+                    count_messages[each] += 1
+                else :
+                    count_messages[each] = 1
+
+            add_space = "\n" + self.__file
+            remove_space = " ".join(add_space.split())
+            split_text = re.split('/, | - ', remove_space)
+            slice_message = split_text[slice(1, len(split_text))]
+
+            new_times = []
+            new_texts = []
+            self.__text_dates = []
+            self.__text_times = []
+            self.__text_authors = []
+            self.__text_messages = []
+
+            for new_format in range(len(slice_message) -1) :
+                text = slice_message[new_format]
+                cut_text = text[-15:]
+                find_times = datefinder.find_dates(cut_text)
+                
+                for match in find_times :
+                    date_format = match.strftime("%m/%d/%Y, %H:%M:%S")
+                    new_times.append(date_format)
+
+                new_texts.append(text[0:len(text)-15+1].strip())
+
+            for time in new_times :
+                split = time.split(", ")
+                dates = split[0]
+                times = split[1]
+
+                self.__text_dates.append(dates)
+                self.__text_times.append(times)
+
+            last_message = slice_message[len(slice_message) -1]
+            new_texts.append(last_message)
+
+            remove_opening = new_texts[slice(1, len(new_texts))]
+
+            for messages in remove_opening :
+                split = messages.split(": ")
+                names = split[0]
+                texts = split[1]
+                
+                self.__text_authors.append(names)
+                self.__text_messages.append(texts)
+
+            data_table = []
+
+            for data in range(len(self.__text_dates)) :
+                data_object = {
+                    "date": self.__text_dates[data],
+                    "time": self.__text_times[data],
+                    "author": self.__text_authors[data],
+                    "message": self.__text_messages[data]
+                }
+
+                data_table.append(data_object)
+
+            return data_table
+
+    def total_chat(self) :
+        if (self.__file[0] == '[') :
+            return 'ios'
+        else :
+            # count day total
+            first_date = datetime.strptime(self.__text_dates[0], '%m/%d/%Y')
+            last_date = datetime.strptime(self.__text_dates[len(self.__text_dates) - 1], '%m/%d/%Y')
+            total_days =  (last_date - first_date).days
+            
+            # messages total
+            messages_total = len(self.__text_messages)
+            
+            # words total
+            words_total = 0
+            for word in self.__text_messages :
+                split_word = word.split(" ")
+                for split_words in split_word :
+                    words_total += 1
+
+            # letters total
+            letters_total = 0
+            for letter in self.__text_messages :
+                letters = letter
+                for split_letter in letters :
+                    letters_total += 1
+
+            data_total_chat = {
+                "total_days": total_days,
+                "messages_total": messages_total,
+                "words_total": words_total,
+                "letters_total": letters_total
+            }
+
+            return data_total_chat
+
+    def content(self) :
+        if (self.__file[0] == '[') :
+            return 'ios'
+        else :
+            # media ommited total
+            media_total = 0
+            for media in self.__text_messages :
+                if media == "<Media omitted>" :
+                    media_total += 1
+
+            # emoji total
+            emoji_total = 0
+            for emojis in self.__text_messages :
+                if emojis in emoji.UNICODE_EMOJI['en'] :
+                    emoji_total += 1
+
+            # link total
+            link_total = 0
+            for link in self.__text_messages :
+                detec_link = link.find("http")
+                if detec_link != -1 :
+                    link_total += 1
+            
+            # gps coordinate
+            gps_total = 0
+            for gps in self.__text_messages :
+                detect_gps = gps.find("location")
+                if detect_gps != -1 :
+                    gps_total += 1
+
+            content_total_chat = {
+                "media_total": media_total,
+                "emoji_total": emoji_total,
+                "link_total": link_total,
+                "gps_total": gps_total
+            }
+
+            return content_total_chat
+
+    def timeline(self) :
+        day_chat = []
+        for day in self.__text_dates :
+            day_name = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday','Sunday']
+            day_number = datetime.strptime(day, '%m/%d/%Y').weekday()
+            days = day_name[day_number]
+            day_chat.append(days)
+
+        timeline_chat = collections.Counter(day_chat)
+        
+        return timeline_chat
---
+++ main.py
@@ -0,0 +1,42 @@
+from io import BytesIO
+import base64
+from flask import Flask, render_template, request, json, Response
+
+from chat_analytic import chatAnalytics
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index() :
+    return render_template('index.html')
+
+@app.route('/result', methods=["GET", "POST"])
+def result() :
+    request_file = request.files['whatsapp_file']
+    
+    if not request_file :
+        return 'bukan file .txt nih, coba lagi ya'
+    
+    try :
+        chat_whatsapp = chatAnalytics(request_file)
+
+        # make a response json
+        response = app.response_class(
+            response = json.dumps({
+                'device_status': chat_whatsapp.check_device(),
+                'detail_chat': chat_whatsapp.detail_chat(),
+                'total_chat': chat_whatsapp.total_chat(),
+                'content_chat': chat_whatsapp.content(),
+                'timeline': chat_whatsapp.timeline()
+            }),
+            status = 200,
+            mimetype = 'application/json'
+        )
+
+        return response
+    except Exception as err :
+        return err
+        # return 'bukan format file dari hasil whatsapp yah'
+
+if __name__ == '__main__' :
+    app.run(debug = True)
